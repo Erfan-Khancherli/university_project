db1bf9b45bc4f43c8eecefc9239290071f1a173d
leo <erfankhancherli7878@gmail.com>
2023-06-05 18:16:52 +0330

    get user name and password def and fix login for now

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def login(self , username , password):
-    self.username = username
-    self.password = password
-    if self.username == 'admin' and self.password == '123456789':
-        return main()
+class Login():
+    def __init__(self , username ,password):
+        self.username = username
+        self.password = password
+    def login(self):
+        if self.username == 'admin' and self.password == '2097':
+            print('Done')
+        else :
+            print('Wrong')
+            return  get_user_pass()
+def get_user_pass():
+    username = input('username :')
+    password = input('password :')
+    return username ,password
+
+username ,password = get_user_pass()
+Login(username, password).login()
+
