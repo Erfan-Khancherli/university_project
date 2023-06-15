edba1980793cb08f16db9380065f291fede2714c
leo <erfankhancherli7878@gmail.com>
2023-06-15 03:48:17 +0330

    main file is for menuand input things now

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
 import os
-from json.decoder import JSONDecodeError
-
-
+from json import JSONDecodeError
+from Transportation import Transportation
+from CrudPackages import CRUD
+from CrudContainers import CRUD1
+from tkinter import *
 class Login():
     def __init__(self , username ,password):
         self.username = username
@@ -19,193 +21,181 @@ def get_user_pass():
     password = input('password :')
     return username ,password
 
-
-
-class Transportation():
-                       
-    class Package():
-        def __init__(self , package_number, weight , destination ,origin , **kwargs):
-            self.package_number = package_number
-            self.weight         = weight
-            self.destination    = destination
-            self.origin         = origin
-            
-            
-    class Breakabl_Packages(Package):
-        def __init__(self, package_number, weight, destination, origin , **kwargs):
-            super().__init__(package_number, weight, destination, origin , **kwargs)
-            pass
-        
-        
-        
-    class cold_Packages(Package) :
-        def __init__(self, package_number, weight, destination, origin ,Minimum_temperature, **kwargs):
-            super().__init__(package_number, weight, destination, origin ,**kwargs)
-            self.Minimum_temperature = Minimum_temperature
-            
-            
-            
-    class Container():
-        def __init__(self , container_number , Maximum_weight , Maximum_package_number , load_package_list ,**kwargs):
-            self.container_number       = container_number
-            self.Maximum_weight         = Maximum_weight
-            self.Maximum_package_number = Maximum_package_number
-            self.load_package_list      = load_package_list
-                
-                
-                
-    class FreezerـContainer(Container):
-        def __init__(self, container_number, Maximum_weight, Maximum_package_number, load_package_list , container_Minimum_temperature , **kwargs):
-            super().__init__(container_number, Maximum_weight, Maximum_package_number, load_package_list , **kwargs)
-            self.container_Minimum_temperature = container_Minimum_temperature
-            
-            
-            
-            
-    class Breakable_Container(Container):
-        def __init__(self, container_number, Maximum_weight, Maximum_package_number, load_package_list , Maximum_speed , **kwargs):
-            super().__init__(container_number, Maximum_weight, Maximum_package_number, load_package_list , **kwargs)
-            self.Maximum_speed = Maximum_speed
-            
-            
-            
-    class Cars():
-        def __init__(self , cars_number , car_Maximum_portable_weight ,**kwargs):
-            self.cars_number             = cars_number
-            self.car_Maximum_portable_weight = car_Maximum_portable_weight
-            
-            
-            
-            
-    class Simple_Cars(Cars):
-        def __init__(self, cars_number, Maximum_portable_weight , Maximum_portable_number , package_list  , **kwargs):
-            super().__init__(cars_number, car_Maximum_portable_weight ,**kwargs)
-            self.Maximum_portable_number = Maximum_portable_number
-            self.package_list            = package_list
-            
-            
-            
-    class Container_Cars(Cars):
-        def __init__(self, cars_number, car_Maximum_portable_weight , Maximum_connectable_container , **kwargs):
-            super().__init__(cars_number, car_Maximum_portable_weight ,**kwargs)
-            self.Maximum_connectable_container = Maximum_connectable_container
-    
-    
-    class CRUD(cold_Packages ,Package,Container):
-        def __init__(self,package_type=None,package_number=1 , weight = None, destination =None, origin = None,container_number = None , Maximum_weight =None, Maximum_package_number=None , load_package_list=None , Minimum_temperature=None):
-            super().__init__(weight = weight, destination = destination, origin = origin , Maximum_weight = Maximum_weight , Maximum_package_number = Maximum_package_number , load_package_list = load_package_list,package_number=package_number , Minimum_temperature=Minimum_temperature,container_number=container_number)
-            self.package_type = package_type
-            
-            
-        def Add_Package(self):
-            self.weight      = input('weight ? :')
-            self.destination = input('maghsad shoma kojast : ')
-            self.origin      = input('mabda shoma kojast : ')
-            self.package_type= input('age baste shoma shekastani ast addad 1 va agar sard ast addad 2 ra vared konid : ')
-            while(1):
-                if package_type =='1' or package_type=='2':
-                    if   package_type =='1' :
-                         package_type = 'Shekastani'
-                    elif package_type =='2':
-                         package_type = 'Sard'
-                         self.Minimum_temperature=input('give me your Minimum temperature :  ')
-                    break
-                else:
-                    print('addad entekhabi shoma bayad 1 ya 2 bashad !')
-                    package_type = input('age baste shoma shekastani ast addad 1 va agar sard ast addad 2 ra vared konid : ')
-            json_list=[]
-            file_name = "./Packages.json"
-            try:    
-                with open(file_name) as f:
-                    data = json.load(f)
-                    json_list=data
-                    self.package_number=json_list[-1]['package_number'] +1
-                    
-                    
-                    dictionary ={
-                        "package_number": self.package_number,
-                        "weight": self.weight,
-                        "destination": self.destination,
-                        "origin" : self.origin,
-                        "package_type" : package_type,
-                        'Minimum_temperature':self.Minimum_temperature,
-                            }
-                    json_list.append(dictionary)
-                    
-                    
-                    
-                    
-                os.remove(file_name)
-                with open(file_name, 'w') as f:
-                    json.dump(json_list, f, indent=4)
-                    
-                    
-                    
-            except JSONDecodeError:
-                
-                dictionary ={
-                            "package_number": self.package_number,
-                            "weight": self.weight,
-                            "destination": self.destination,
-                            "origin" : self.origin,
-                            "package_type" : package_type,
-                            'Minimum_temperature':self.Minimum_temperature,
-                                }
-                json_list.append(dictionary)
-                
-                
-                
-                os.remove(file_name)
-                with open(file_name, 'w') as f:
-                    json.dump(json_list, f, indent=4)
-                    
-        def edit_input(self , value_to_edit ,weight ,destination ,origin , package_type , Minimum_temperature ):
-            if value_to_edit == 'weight':
-                self.weight= input('vazn jadid ra vared konid : ')
-            if value_to_edit == 'destination':
-                self.destination= input('maghsad jadid ra vared konid : ')
-            if value_to_edit == 'origin':
-                self.origin =input('mabda jadid ra vared konid : ') 
-            if value_to_edit == 'package_type':
-                self.package_type = input('package_type jadid ra vared konid : ')
-            if value_to_edit == 'Minimum_temperature':
-                self.Minimum_temperature =  input('Minimum_temperature jadid ra vared konid : ')    
-            return self.weight ,self.destination ,self.origin , self.package_type , self.Minimum_temperature           
-        def Edit_Packages(self):
-            file_name = "./Packages.json"
-            try:
-                with open(file_name) as f:
-                    data = json.load(f)
-                    for i in range (len(data)):
-                        print(data[i])
-            except JSONDecodeError:
-                print('There is no packages')
-            self.package_number= int(input('give me your package_number that you need to Edit : '))
-            x = data[self.package_number-1]['package_number']
-            print(list(data[self.package_number-1].keys()))
-            value_to_edit=input("which you want to change ?!!! : ")
-            self.weight               =data[self.package_number-1]['weight'] 
-            self.destination          =data[self.package_number-1]['destination'] 
-            self.origin               =data[self.package_number-1]['origin'] 
-            self.package_type         =data[self.package_number-1]['package_type'] 
-            self.Minimum_temperature  =data[self.package_number-1]['Minimum_temperature'] 
-            self.weight , self.destination , self.origin , self.package_type , self.Minimum_temperature = self.edit_input(value_to_edit,self.weight , self.destination , self.origin , self.package_type , self.Minimum_temperature)
-            data[self.package_number-1]['weight'] = self.weight
-            data[self.package_number-1]['destination'] = self.destination
-            data[self.package_number-1]['origin'] = self.origin
-            data[self.package_number-1]['package_type'] = self.package_type
-            data[self.package_number-1]['Minimum_temperature'] = self.Minimum_temperature
-            os.remove(file_name)
-            with open(file_name, 'w') as f:
-                json.dump(data, f, indent=4)
-            edit_again  = input('do you want to change more ? (Y/N)')
-            if edit_again == 'Y' or edit_again=='y':
-                self.Edit_Packages()
-            elif edit_again == 'N' or edit_again == 'n':
-                pass
-        
-            
 # username ,password = get_user_pass()
 # Transportation(username, password).login()
 # Transportation.CRUD(destination = 80).Add_Package()
-Transportation.CRUD(destination = 80).Edit_Packages()
+# CRUD('1' , 30 , 'qom' , 'tehran' , 30)
+
+if __name__ == '__main__':
+    packageCrud = CRUD()
+    containerCrud = CRUD1()
+    AdminMenu          = ['1-Crud packages' , '2-Crud Containers' , '3-Crud Cars' , '4-Loading' , '5-Send and Recive' , '6-Exit']
+    CrudPackagesMenu   = ['1-Add package' , '2-edit package' , '3-Delete package']
+    CrudContainersMenu = ['1-Add container' , '2-edit container' , '3-Delete container']
+    Packages_type      = ['1-Breakable' , '2-Cold' , '3-Default']
+    Container_type     = ['1-Breakable' , '2-Freezer']
+    packageCrud.load_from_file()
+    containerCrud.load_from_file()
+    # top = Tk()
+    
+    
+    # crud1 = Button(top, text = "packages").grid(row = 0, column = 0) 
+    # crud2 = Button(top, text = "Containers").grid(row = 1, column = 0) 
+    # crud3 = Button(top, text = "Cars").grid(row = 2, column = 0)  
+    
+    
+    # top.mainloop()
+    # pass
+    while(True):
+        for i in AdminMenu:
+            print(i)
+        AdminSelector = input('enter the number : ')
+        if AdminSelector =='1':
+            
+            for i in CrudPackagesMenu:
+                print(i)
+            CrudSelector = input('enter you number : ')
+            
+            if CrudSelector == '1':
+                type_ = ""
+                for i in Packages_type:
+                    print(i)
+                package_type = input('enter you number : ')
+                Weight       = input('Weight :')
+                Destination  = input('Destination : ')
+                Origin       = input('Origin : ')
+                file_name = "./Packages.json"
+                try:
+                    temp = 0
+                    with open(file_name) as f:
+                        data = json.load(f)
+                        for i in range(len(data)):
+                            if data[i-1]['package_number']>temp:
+                                temp = data[i-1]['package_number']
+                        package_number=temp +1
+                except JSONDecodeError:
+                    package_number = 1
+                if package_type == '1':
+                    type_ = "breakable"
+                    package = Transportation.Package.create_package(type_,package_number, Weight, Destination, Origin)
+                    print(package)
+                    packageCrud.add(package)
+                    packageCrud.save()
+                elif package_type =='2':
+                    type_ = "cold"
+                    Minimum_temperature = input('Minimum_temperature : ')
+                    package = Transportation.Package.create_package(type_,package_number, Weight, Destination, Origin , Minimum_temperature)
+                    packageCrud.add(package)
+                    packageCrud.save()
+                    
+                elif package_type =='3':
+                    type_ = "Default"
+                    package = Transportation.Package.create_package(type_,package_number, Weight, Destination, Origin)
+                    packageCrud.add(package)   
+            elif CrudSelector == '2':
+                packageCrud.show()
+                packageEditNumber = int(input('witch package you want to edit(give me your package number) : '))
+                packageCrud.target(packageEditNumber)
+                packageEditParam = input('give me the number of param you want to change : ')
+                while(True):
+                    if packageEditParam == '1' or packageEditParam=='2' :
+                        print('\nyou cant change package number or package type or !!! \n')
+                        packageEditParam = input('give me the number of param you want to change : ')
+                    elif packageEditParam >'6' :
+                        print('\nwrong number !!!\n')
+                        packageEditParam = input('give me the number of param you want to change : ')
+                    else:
+                        break
+                packageEditParamValue= input('give me you param for change : ')
+                packageCrud.update(packageEditParam,packageEditParamValue,packageEditNumber)
+                packageCrud.save()
+                
+            elif CrudSelector == '3':
+                packageCrud.show()
+                DeletePackageNumber = int(input('witch package you want to delete : '))
+                packageCrud.delete(DeletePackageNumber)
+                packageCrud.save()
+                
+        elif AdminSelector =='2':
+            for i in CrudContainersMenu:
+                print(i)
+            CrudSelector = input('enter you number : ')
+            
+            if CrudSelector == '1':
+                type_ = ""
+                for i in Container_type:
+                    print(i)
+                container_type            = input('enter you number : ')
+                Maximum_weight            = input('Maximum_weight :')
+                Maximum_package_number    = input('Maximum_package_number : ')
+                file_name = "./Containers.json"
+                try:
+                    temp = 0
+                    with open(file_name) as f:
+                        data = json.load(f)
+                        for i in range(len(data)):
+                            if data[i-1]['container_number']>temp:
+                                temp = data[i-1]['container_number']
+                        container_number=temp +1
+                except JSONDecodeError:
+                    container_number = 1
+                if container_type == '1':
+                    type_ = "breakable"
+                    Maximum_speed = input('Maximum_speed : ')
+                    container = Transportation.Container.create_container(type_,container_number, Maximum_weight, Maximum_package_number, Maximum_speed)
+                    containerCrud.add(container)
+                    containerCrud.save()
+                elif container_type =='2':
+                    type_ = "freezer"
+                    container_Minimum_temperature = input('Minimum_temperature : ')
+                    container = Transportation.Container.create_container(type_,container_number, Maximum_weight, Maximum_package_number,container_Minimum_temperature)
+                    containerCrud.add(container)
+                    containerCrud.save()  
+            elif CrudSelector == '2':
+                containerCrud.show()
+                ContainerEditNumber = int(input('witch container you want to edit(give me your container number) : '))
+                containerCrud.target(ContainerEditNumber)
+                ContainerEditParam = input('give me the number of param you want to change : ')
+                while(True):
+                    if ContainerEditParam == '1' or ContainerEditParam=='2' or ContainerEditParam=='5':
+                        print('\nyou cant change container number or container type or container load package list!!! \n')
+                        ContainerEditParam = input('give me the number of param you want to change : ')
+                    elif ContainerEditParam >'6' :
+                        print('\nwrong number !!!\n')
+                        ContainerEditParam = input('give me the number of param you want to change : ')
+                    else:
+                        break
+                ContainerEditParamValue= input('give me you param for change : ')
+                containerCrud.update(ContainerEditParam,ContainerEditParamValue,ContainerEditNumber)
+                containerCrud.save()
+                
+                
+            elif CrudSelector == '3':
+                containerCrud.show()
+                DeleteContainerNumber = int(input('witch package you want to delete : '))
+                containerCrud.delete(DeleteContainerNumber)
+                containerCrud.save() 
+        elif AdminSelector == '3':
+            pass
+        elif AdminSelector =='4':
+            pass
+        elif AdminSelector == '5':
+            pass
+        elif AdminSelector == '6':
+            pass
+    
+    # package_type=input('package_type : ')
+    # weight = input('weight : ')
+    # destination = input('destination : ')
+    # origin = input('origin : ')
+    # crud = CRUD()
+    # ##input package type
+    # if package_type =='1':
+    #     crud.package = Transportation.Breakabl_Packages(weight ,destination ,origin)
+    # elif package_type == '2':
+    #     Minimum_temperature = input('Minimum_temperature : ')
+    #     crud.package = Transportation.cold_Packages(weight ,destination ,origin , Minimum_temperature)
+    # else :
+    #     crud.package = Transportation.Package(weight , destination ,origin)
+
+    
